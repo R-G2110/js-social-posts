f1caefc746c36e84cd0ab5a08f9f5cc133fa03bb
R-G2110 <argeldelacruz86@gmail.com>
2023-09-25 15:06:32 +0200

    [Add]: posts on index with script (to be edited)

--- js/script.js
+++ js/script.js
@@ -55,3 +55,40 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+//MILESTONE 2
+const post = document.querySelector('#container');
+for( let i = 0; i < posts.length; i++){
+    post.innerHTML += `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${posts[i].author.image}"     alt="${posts[i].author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${posts[i].author.name}</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${posts[i].content}</div>
+        <div class="post__image">
+            <img src="${posts[i].media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#"     data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up"   aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a ${posts[i].likes}</    b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+}
