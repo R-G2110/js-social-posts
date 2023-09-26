3fa89e4d95b9933661aaa1c9a43ba502cf2cea86
R-G2110 <argeldelacruz86@gmail.com>
2023-09-26 19:06:46 +0200

    [Add]: correction on script

--- js/script.js
+++ js/script.js
@@ -1,95 +1,109 @@
-const posts = [
-    {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
-        },
-        "likes": 80,
-        "created": "2021-06-25"
-    },
-    {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
-        },
-        "likes": 120,
-        "created": "2021-09-03"
-    },
-    {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
-        },
-        "likes": 78,
-        "created": "2021-05-15"
-    },
-    {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
-        },
-        "likes": 56,
-        "created": "2021-04-03"
-    },
-    {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
-        },
-        "likes": 95,
-        "created": "2021-03-05"
-    }
-];
+import { posts } from "./db.js"; 
 
-//Stampare le stesse informazioni su DOM sottoforma di stringhe
-//MILESTONE 2
-const post = document.querySelector('#container');
-for( let i = 0; i < posts.length; i++){
-    post.innerHTML += `
+let userLike = [];
+
+const container = document.getElementById('container');
+
+posts.forEach( post => container.innerHTML += getPostTemplate(post) )
+
+
+
+/************  FUNCTIONS *************/
+
+function getPostTemplate(post){
+    const {id, content, media, author, likes, created} = post;
+    return `
     <div class="post">
-        <div class="post__header">
-            <div class="post-meta">                    
-                <div class="post-meta__icon">
-                    <img class="profile-pic" src="${posts[i].author.image}"     alt="${posts[i].author.name}">                    
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                       ${author.image ? getAuthorImage(author) :  getAuthorInitials(author)}
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${author.name}</div>
+                        <div class="post-meta__time">${getDateFormat(created)}</div>
+                    </div>                    
                 </div>
-                <div class="post-meta__data">
-                    <div class="post-meta__author">${posts[i].author.name}</div>
-                    <div class="post-meta__time">4 mesi fa</div>
-                </div>                    
             </div>
+            <div class="post__text">${content}</div>
+            <div class="post__image">
+                <img src="${media}" alt="${author.name}">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button ${isPostLike(id) ? 'like-button--liked' : ''}" href="#">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
         </div>
-        <div class="post__text">${posts[i].content}</div>
-        <div class="post__image">
-            <img src="${posts[i].media}" alt="">
-        </div>
-        <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#"     data-postid="1">
-                        <i class="like-button__icon fas fa-thumbs-up"   aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
-                </div>
-                <div class="likes__counter">
-                    Piace a ${posts[i].likes}</    b> persone
-                </div>
-            </div> 
-        </div>            
-    </div>`
+    
+    `
 }
 
+function isPostLike(id){
+    return userLike.includes(id);
+}
+
+function getDateFormat(date){
+    return date.split('-').reverse().join('/');
+}
+
+function getAuthorImage(author){
+    const {name, image} = author
+    return `<img class="profile-pic" src="${image}" alt="${name}"> `
+}
+
+function getAuthorInitials(author){
+    const { name } = author;
+
+    const letters = name.split(' ').map( (nameSplit) => {
+        console.log(nameSplit);
+        console.log(nameSplit.charAt(0));
+        return nameSplit.charAt(0)
+    } );
+
+    console.log(letters);
+
+    const initials = letters.join('');
+
+    return `<div class="profile-pic-default">
+                <span>${initials}</span>
+            </div>`
+}
+
+// prendo tutti i bottoni ed i contatori dei like
+const likeButtons = document.querySelectorAll('.js-like-button');
+const likeCounters = document.querySelectorAll('.js-likes-counter');
+
+// ciclo per mettere l'eventListener ai bottoni
+likeButtons.forEach((btn, index) => {
+    btn._id = posts[index].id;
+    btn._index = index;
+    btn.addEventListener('click', handlerLikeBtn)
+})
+
+
+function handlerLikeBtn(event){
+
+    event.preventDefault();
+    this.classList.toggle('like-button--liked');
+    const postSelected = posts.find( post => post.id === this._id);
+
+    if(userLike.includes(this._id)){
+        userLike = userLike.filter( likeId => likeId !==  this._id);
+        postSelected.likes--;
+    } else {
+
+        postSelected.likes++;
+        userLike.push(this._id);
+    }
+
+    likeCounters[this._index].innerText = postSelected.likes;
+}
